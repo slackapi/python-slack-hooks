b8dd2326adec7c7a3cdde47986226b7ea0be0655
William Bergamin <wbergamin@slack-corp.com>
2023-12-06 13:10:43 -0500

    Simplify socketmode mock

--- tests/scenario_test/test_app/app.py
+++ tests/scenario_test/test_app/app.py
@@ -11,6 +11,15 @@ web_client = WebClient(base_url="http://localhost:8888", token=os.environ.get("S
 
 app = App(signing_secret="valid", client=web_client)
 
+result = {"shortcut": False}
+
+
+@app.shortcut("do-something")
+def shortcut_handler(ack):
+    result["shortcut"] = True
+    ack()
+
+
 if __name__ == "__main__":
     print("ran as __main__")
     handler = get_test_socket_mode_handler(3012, app, os.environ.get("SLACK_APP_TOKEN"))
