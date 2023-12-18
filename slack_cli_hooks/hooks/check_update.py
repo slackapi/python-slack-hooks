49633eb88e363387d7f59ba5bf6e9c5cb6fa2bda
William Bergamin <wbergamin@slack-corp.com>
2023-12-18 16:53:17 -0500

    add payload extraction

--- slack_cli_hooks/hooks/check_update.py
+++ slack_cli_hooks/hooks/check_update.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import json
+from typing import Any, Dict
 from urllib import request
 
 from slack_cli_hooks.protocol import Protocol, build_protocol
+from slack_cli_hooks.error import CliError
 from http.client import HTTPResponse, HTTPMessage
 
-PROTOCOL: Protocol
+PROTOCOL: Protocol = build_protocol()
 
 
 class PypiResponse:
-    def __init__(self, status: int, headers: HTTPMessage, body: str):
+    def __init__(self, url: str, status: int, headers: HTTPMessage, body: str):
+        self.url = url
         self.status = status
         self.headers = headers
         self.body = body
@@ -21,7 +24,27 @@ def get_pypi_response(project: str) -> PypiResponse:
     pypi_request = request.Request(method="GET", url=url, headers={"Accept": "application/json"})
     response: HTTPResponse = request.urlopen(pypi_request)
     charset = response.headers.get_content_charset() or "utf-8"
-    return PypiResponse(status=response.getcode(), headers=response.headers, body=response.read().decode(charset))
+    return PypiResponse(
+        url=response.url, status=response.getcode(), headers=response.headers, body=response.read().decode(charset)
+    )
+
+
+def get_pypi_json(project: str) -> Dict[str, Any]:
+    pypi_response = get_pypi_response(project)
+    if pypi_response.status > 200:
+        PROTOCOL.debug(f"Received status {pypi_response.status} from {pypi_response.url}")
+        PROTOCOL.debug(f"Headers {pypi_response.headers.items()}")
+        PROTOCOL.debug(f"Body {pypi_response.body}")
+        raise CliError(f"Received status {pypi_response.status} from {pypi_response.url}")
+    return json.loads(pypi_response.body)
+
+
+def extract_latest_version(payload: Dict[str, Any]) -> int:
+    if "info" not in payload:
+        raise CliError("Missing `info` field in pypi payload")
+    if "version" not in payload["info"]:
+        raise CliError("Missing `version` field in pypi payload['info']")
+    return payload["info"]["version"]
 
 
 example_output = {
@@ -45,6 +68,5 @@ example_output = {
 
 
 if __name__ == "__main__":
-    PROTOCOL = build_protocol()
     resp = get_pypi_response("slack-bolt")
     PROTOCOL.respond(json.dumps({"status": resp.status, "headers": resp.headers, "body": json.loads(resp.body)}))
--- tests/slack_cli_hooks/hooks/test_chek_update.py
+++ tests/slack_cli_hooks/hooks/test_chek_update.py
@@ -1,11 +1,19 @@
 from http.client import HTTPMessage
+import json
 from unittest import mock
 
-from slack_cli_hooks.hooks.check_update import get_pypi_response
+import pytest
+from slack_cli_hooks.error import CliError
+
+from slack_cli_hooks.hooks.check_update import PypiResponse, extract_latest_version, get_pypi_response, get_pypi_json
+
+
+def resolve_module(func) -> str:
+    return f"slack_cli_hooks.hooks.check_update.{func.__name__}"
 
 
 class TestGetManifest:
-    def test_find_file_path(self):
+    def test_get_pypi_response(self):
         mock_headers = HTTPMessage()
         mock_headers.add_header("Content-Type", 'application/json; charset="UTF-8"')
 
@@ -20,3 +28,59 @@ class TestGetManifest:
 
         assert response.status == 200
         assert response.body == "{}"
+
+    def test_get_pypi_json(self):
+        project = "my_test_project"
+        mock_headers = HTTPMessage()
+        mock_headers.add_header("Content-Type", 'application/json; charset="UTF-8"')
+
+        mock_response = PypiResponse(
+            url="https://pypi.org/pypi/{project}/json",
+            status=200,
+            headers=mock_headers,
+            body=json.dumps({"info": {}, "releases": {}}),
+        )
+
+        with mock.patch(resolve_module(get_pypi_response)) as mock_get_pypi_response:
+            mock_get_pypi_response.return_value = mock_response
+            json_response = get_pypi_json(project)
+
+        assert json_response == {"info": {}, "releases": {}}
+
+    def test_get_pypi_json_fail(self):
+        project = "my_test_project"
+        mock_headers = HTTPMessage()
+        mock_headers.add_header("Content-Type", 'application/json; charset="UTF-8"')
+
+        mock_response = PypiResponse(
+            url="https://pypi.org/pypi/{project}/json",
+            status=300,
+            headers=mock_headers,
+            body=json.dumps({"info": {}, "releases": {}}),
+        )
+
+        with mock.patch(resolve_module(get_pypi_response)) as mock_get_pypi_response:
+            mock_get_pypi_response.return_value = mock_response
+            with pytest.raises(CliError) as e:
+                get_pypi_json(project)
+
+            assert "300" in str(e)
+            assert "https://pypi.org/pypi/{project}/json" in str(e)
+
+    def extract_latest_version(self):
+        test_payload = {"info": {"version": "0.0.0"}}
+        actual = extract_latest_version(test_payload)
+        assert actual == "0.0.0"
+
+    def extract_latest_version_missing_info(self):
+        test_payload = {}
+        with pytest.raises(CliError) as e:
+            extract_latest_version(test_payload)
+            assert "info" in str(e)
+
+    def extract_latest_version_missing_version(self):
+        test_payload = {"info": {}}
+        with pytest.raises(CliError) as e:
+            extract_latest_version(test_payload)
+            assert "version" in str(e)
+            assert "payload['info']" in str(e)
