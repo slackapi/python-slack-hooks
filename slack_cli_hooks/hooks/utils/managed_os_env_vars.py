672d56d9fdffd7d9acb5919deb4c6196c17cfa8e
William Bergamin <wbergamin@slack-corp.com>
2023-12-21 12:16:03 -0500

    Update managed_os_env_vars.py

--- slack_cli_hooks/hooks/utils/managed_os_env_vars.py
+++ slack_cli_hooks/hooks/utils/managed_os_env_vars.py
@@ -10,7 +10,9 @@ class ManagedOSEnvVars:
 
     def set_if_absent(self, os_env_var: str, value: str) -> None:
         if os_env_var in os.environ:
-            self._protocol.info(f"{os_env_var} environment variable detected in session, using it over the provided one!")
+            self._protocol.info(
+                f"{os_env_var} environment variable detected in session, using it over the provided one!"
+            )
             return
         self._os_env_vars.append(os_env_var)
         os.environ[os_env_var] = value
