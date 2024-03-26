2a87bd3a3f6e0bdd03b5b8d4fc0c21dc5f1718e5
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-03-25 19:24:11 -0700

    test(style): compare strings using string comparison operators

--- tests/slack_cli_hooks/hooks/test_doctor.py
+++ tests/slack_cli_hooks/hooks/test_doctor.py
@@ -8,9 +8,9 @@ class TestDoctor:
         versions = doctor_payload.get("versions")
         assert versions is not None
 
-        assert versions[0].get("name") is "python"
-        assert versions[0].get("current") is platform.python_build()[0]
-        assert versions[1].get("name") is "implementation"
-        assert versions[1].get("current") is platform.python_implementation()
-        assert versions[2].get("name") is "compiler"
-        assert versions[2].get("current") is platform.python_compiler()
+        assert versions[0].get("name") == "python"
+        assert versions[0].get("current") == platform.python_build()[0]
+        assert versions[1].get("name") == "implementation"
+        assert versions[1].get("current") == platform.python_implementation()
+        assert versions[2].get("name") == "compiler"
+        assert versions[2].get("current") == platform.python_compiler()
