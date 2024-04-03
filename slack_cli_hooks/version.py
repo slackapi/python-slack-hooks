d9e93afb39e1e6d322abdc2e2b8f9a6039a0b8ed
William Bergamin <wbergamin@salesforce.com>
2024-04-03 10:23:59 -0400

    feat: automate releases (#36)
    * Automate release
    
    * Update maintainers_guide.md
    
    * Improve wording

--- slack_cli_hooks/version.py
+++ slack_cli_hooks/version.py
@@ -1,2 +1,5 @@
 """Check the latest version at https://pypi.org/project/slack-cli-hooks/"""
-__version__ = "0.0.0.dev2"
+
+import os
+
+__version__ = os.environ.get("SLACK_CLI_HOOKS_VERSION", "0.0.0.dev0")
