f1647b9b2c7c6f2668f9993021af51edabbddb54
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-07-14 15:43:48 -0700

    docs: replace links to the slack cli page with current tools documentation

--- slack_cli_hooks/__init__.py
+++ slack_cli_hooks/__init__.py
@@ -1,7 +1,7 @@
 """
-A Slack CLI hooks implementation in Python to build Bolt Slack apps leveraging the full power of the [Slack CLI](https://api.slack.com/automation/cli/install). Look at our [code examples](https://github.com/slackapi/python-slack-hooks/tree/main/examples) to learn how to build apps using the SLack CLI and Bolt.
+A Slack CLI hooks implementation in Python to build Bolt Slack apps leveraging the full power of the [Slack CLI](https://tools.slack.dev/slack-cli/). Look at our [code examples](https://github.com/slackapi/python-slack-hooks/tree/main/examples) to learn how to build apps using the SLack CLI and Bolt.
 
-* Slack CLI: https://api.slack.com/automation/cli/install
+* Slack CLI: https://tools.slack.dev/slack-cli/
 * Bolt Website: https://slack.dev/bolt-python/
 * GitHub repository: https://github.com/slackapi/python-slack-hooks
 """  # noqa: E501
