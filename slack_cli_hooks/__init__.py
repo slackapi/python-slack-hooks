0bcc92b74fd282026725024188fdd68e71d6b0a5
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-07-15 12:52:02 -0700

    docs: reference the quickstart for building apps with the cli and bolt (#60)

--- slack_cli_hooks/__init__.py
+++ slack_cli_hooks/__init__.py
@@ -1,5 +1,5 @@
 """
-A Slack CLI hooks implementation in Python to build Bolt Slack apps leveraging the full power of the [Slack CLI](https://tools.slack.dev/slack-cli/). Look at our [code examples](https://github.com/slackapi/python-slack-hooks/tree/main/examples) to learn how to build apps using the SLack CLI and Bolt.
+A Slack CLI hooks implementation in Python to build Bolt Slack apps leveraging the full power of the [Slack CLI](https://tools.slack.dev/slack-cli/). Read the [quickstart](https://tools.slack.dev/bolt-python/getting-started/) to learn how to build apps using the Slack CLI and Bolt.
 
 * Slack CLI: https://tools.slack.dev/slack-cli/
 * Bolt Website: https://slack.dev/bolt-python/
